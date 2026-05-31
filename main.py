77287eda844fbad712c704629741cbb4ce3840b9
unknown <once6474@naver.com>
2026-05-31 12:43:43 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, Response
+from fastapi.responses import HTMLResponse, Response, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
@@ -37,6 +37,11 @@ class FavoriteItem(BaseModel):
     description: str = ""
     link: str
     thumbnail: str = ""
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 
 @app.get("/", response_class=HTMLResponse)
